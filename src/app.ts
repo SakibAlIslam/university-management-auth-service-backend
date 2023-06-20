c653c0d82c43f01356f596f8b6f3a2f76a4d5bb8
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-06-20 07:07:24 +0600

    [DONE] setup cors, parser

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 
 const app: Application = express()
-const port = 3000
+const port = 3000;
+
+app.use(cors);
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
